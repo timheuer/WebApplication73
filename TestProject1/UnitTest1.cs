87a105b7e0a1fff62ca89bffe48e8afbda6be196
Tim Heuer <tim@timheuer.com>
2021-06-10 11:41:20 -0700

    Add project files.

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,32 @@
+using Microsoft.Playwright;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions() { Headless = false });
+
+            var context = await browser.NewContextAsync(new BrowserNewContextOptions()
+            {
+                Locale = "de-DE"
+            });
+
+            var page = await context.NewPageAsync();
+            await page.GotoAsync("http://localhost:51511/");// https://tacticsview.azurewebsites.net");
+            await page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+            await page.ClickAsync("#navlink-consider");
+            await page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+            await page.ScreenshotAsync(new PageScreenshotOptions() { Path = "consider-german.png" });
+            await page.ClickAsync("#navlink-approved");
+            await page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+            await page.ScreenshotAsync(new PageScreenshotOptions() { Path = "approved-german.png" });
+        }
+    }
+}
