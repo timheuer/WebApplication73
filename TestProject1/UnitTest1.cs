2cc0a1df96cdd7b53fdc81e717339eddda19e251
Tim Heuer <tim@timheuer.com>
2021-06-10 11:57:21 -0700

    change network loadstate

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -20,12 +20,12 @@ namespace TestProject1
 
             var page = await context.NewPageAsync();
             await page.GotoAsync("https://tacticsview.azurewebsites.net");
-            await page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+            await page.WaitForLoadStateAsync(LoadState.DOMContentLoaded);
             await page.ClickAsync("#navlink-consider");
-            await page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+            await page.WaitForLoadStateAsync(LoadState.DOMContentLoaded);
             await page.ScreenshotAsync(new PageScreenshotOptions() { Path = "screenshots/consider-german.png" });
             await page.ClickAsync("#navlink-approved");
-            await page.WaitForLoadStateAsync(LoadState.NetworkIdle);
+            await page.WaitForLoadStateAsync(LoadState.DOMContentLoaded);
             await page.ScreenshotAsync(new PageScreenshotOptions() { Path = "screenshots/approved-german.png" });
         }
     }
